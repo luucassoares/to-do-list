98ed349f3d8a34fa8b699079501b56d510d1aedf
Lucas Soares <luucassoares94@gmail.com>
2023-10-02 14:22:13 -0300

    initial commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Lucas Soares on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
